ce14dc0cbaeae8eb4d32d08508d095dc8c8c4197
Douglas Adams <douglas.adams@me.com>
2021-11-16 13:09:32 -0500

    Initial Commit

---
+++ TestDiscoveryApp/TestDiscoveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDiscoveryAppApp.swift
+//  TestDiscoveryApp
+//
+//  Created by Douglas Adams on 11/16/21.
+//
+
+import SwiftUI
+
+@main
+struct TestDiscoveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
