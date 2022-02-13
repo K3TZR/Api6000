15f4fa9f0cc2a914a6504fef35cc225a1eca5943
Douglas Adams <douglas.adams@me.com>
2022-02-12 23:32:57 -0500

    v2.1.3

--- Api6000/Api6000App.swift
+++ Api6000/Api6000App.swift
@@ -12,14 +12,14 @@ import ApiViewer
 import Shared
 
 final class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        // disable tab view
-        NSWindow.allowsAutomaticWindowTabbing = false
-    }
-    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        // close when last window closed
-        true
-    }
+  func applicationDidFinishLaunching(_ notification: Notification) {
+    // disable tab view
+    NSWindow.allowsAutomaticWindowTabbing = false
+  }
+  func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    // close when last window closed
+    true
+  }
 }
 
 @main
@@ -28,23 +28,18 @@ struct Api6000App: App {
   var appDelegate
   
   var body: some Scene {
-
-    let bundleIdentifier = Bundle.main.bundleIdentifier ?? "someDomain.SomeApp"
-    let separator = bundleIdentifier.lastIndex(of: ".")!
-    let appName = String(bundleIdentifier.suffix(from: bundleIdentifier.index(separator, offsetBy: 1)))
-    let domain = String(bundleIdentifier.prefix(upTo: separator))
-
+    
     WindowGroup {
       ApiView(
         store: Store(
-          initialState: ApiState(domain: domain, appName: appName),
+          initialState: ApiState(),
           reducer: apiReducer,
           environment: ApiEnvironment()
         )
       )
-      .navigationTitle(appName + "   v" + Version().string)
-      .frame(minWidth: 975, minHeight: 400)
-      .padding()
+        .navigationTitle(getBundleInfo().appName + "   v" + Version().string)
+        .frame(minWidth: 975, minHeight: 400)
+        .padding()
     }
     .commands {
       //remove the "New" menu item
