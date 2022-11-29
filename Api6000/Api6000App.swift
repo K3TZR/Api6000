319e7da7693970abb4ff94c0cb64d5897f548028
Douglas Adams <douglas.adams@me.com>
2022-11-28 19:46:17 -0500

    Initial Commit

---
+++ Api6000/Api6000App.swift
@@ -0,0 +1,17 @@
+//
+//  Api6000App.swift
+//  Api6000
+//
+//  Created by Douglas Adams on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Api6000App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
